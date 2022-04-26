ecd89a6c65ed0cc86fe0550decf385afab91b4b8
Zesheng Xing <44153625+zsxing99@users.noreply.github.com>
2022-04-26 14:14:06 -0700

    -updated BE dev

---
+++ backend/configs/swagger.js
@@ -0,0 +1,11 @@
+module.exports = {
+    info: {
+        version: '0.1.0',
+        title: 'CMU-SV UX Analytics',
+    },
+    baseDir: __dirname,
+    // Glob pattern to find your jsdoc files (multiple patterns can be added in an array)
+    filesPattern: '../routes/*.js',
+    swaggerUIPath: '/api-docs',
+    exposeSwaggerUI: true,
+};
